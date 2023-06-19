6338c4ceed36125559254a0ad39639c4cbad3147
Simon-Speed <95simon.speed@gmail.com>
2023-06-19 16:44:45 +0100

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import './scss/core.scss';
+import React, { Component } from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="App-background">
+      <h1 className="Title">Title</h1>
+      <div 
+        className="component-bg" 
+        draggable
+        onDragStart={this.handleDragStart}
+        onDrag={this.handleDrag}
+        onDragEnd={this.handleDragEnd}
         >
-          Learn React
-        </a>
-      </header>
+          Content
+      </div>
     </div>
   );
 }
